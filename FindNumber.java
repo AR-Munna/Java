63b81c4482a994c0fed2c594737379e060a673ee
AR-Munna <armunna1997@gmail.com>
2017-04-19 12:13:55 +0600

    Add files via upload

---
+++ FindNumber.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lab09;
+
+/**
+ *
+ * @author Student
+ */
+public class FindNumber {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
