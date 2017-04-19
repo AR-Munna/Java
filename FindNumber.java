8f0f773b9e2f526e38e80718e872718c14223c70
AR-Munna <armunna1997@gmail.com>
2017-04-19 12:15:03 +0600

    Add files via upload

--- FindNumber.java
+++ FindNumber.java
@@ -1,21 +1,84 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+package package1;
 
-package lab09;
+import java.io.InputStream;
+import java.util.Random;
+import java.util.*;
 
-/**
- *
- * @author Student
- */
-public class FindNumber {
+public class FindNumber extends Thread {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+
+
+    private int lo, hi;
+    private int[] arr;
+    private int find;
+    private boolean flag;
+
+    public FindNumber(int[] arr, int lo, int hi, int find) {
+        this.lo = lo;
+        this.hi = hi;
+        this.arr = arr;
+        this.find = find;
+        flag = false;
     }
 
+    @Override
+    public void run() {
+        for (int i = lo; i < hi; i++) {
+            if(arr[i]==find) 
+            {
+               flag = true;
+               break;
+            }
+            else flag = false;
+        }
+    }
+
+    public static boolean findInt(int[] arr, int f) throws InterruptedException {
+        int len = arr.length;
+        boolean check = false;
+        boolean check1 = false;
+
+       
+        FindNumber[] ts = new FindNumber[5];
+        for (int i=0; i<5; i++) {
+            ts[i] = new FindNumber(arr, (i * len) / 5, ((i + 1) * len / 5), f);
+            ts[i].start();
+            ts[i].join();
+            check = ts[i].flag;            
+            if(check==true)
+            {
+                check1 = true;
+                break;
+            }
+        }
+        return check1;
+    }
+
+    public static void main(String[] args) throws InterruptedException
+    {
+        
+        int[] arr = new int[50];
+        int f;
+
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < arr.length; i++) {
+            Random rand = new Random();
+            arr[i] = rand.nextInt(100) ;
+            System.out.print(arr[i]+" ");
+        }
+        
+         System.out.println();
+
+        f = sc.nextInt();
+        try{
+               boolean find = findInt(arr, f);
+               if(find == true)System.out.println(f+" is found");
+               else System.out.println(f+" Not found");
+        }
+        catch(InterruptedException e)
+        {
+            System.out.println("Munna");
+        }       
+    }
 }
